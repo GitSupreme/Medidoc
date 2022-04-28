0dd40b2549b6ab05b7d955210b83537a8c3f5d56
GitSupreme <amakyejephtha@gmail.com>
2022-04-28 22:36:51 +0000

    negilgible change

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Medidoc</Text>
+      <Text>Medidoc mobile applcation</Text>
       <StatusBar style="auto" />
     </View>
   );
